a55e9a41c5ea0133025804e448ffc252d8ca3375
Priti Mahajan <156574230+PritiHM@users.noreply.github.com>
2025-11-18 16:50:01 +0530

    updated ui and fixes

--- components/JoinWaitList.jsx
+++ components/JoinWaitList.jsx
@@ -10,8 +10,7 @@ export default function JoinWaitList() {
   return (
     <section
       id="contact"
-      className=" w-full max-w-[1250px]  mx-auto  px-4 md:px-8 bg-[#00656D] text-[#F7F6F1] py-16 mt-10 mb-10 rounded-xl relative overflow-hidden
-      "
+      className="w-full max-w-[1250px] mx-auto px-4 md:px-8 bg-[#00656D] text-[#F7F6F1] py-16 mt-10 mb-10 rounded-xl relative overflow-hidden"
     >
       {/* Corner Image */}
       <div className="pointer-events-none absolute top-0 right-0 -translate-y-3 z-20">
@@ -22,10 +21,9 @@ export default function JoinWaitList() {
         />
       </div>
 
-     
       <div className="grid grid-cols-1 md:grid-cols-[60%_40%] gap-10 items-start">
         
-      
+        {/* Left Side Content */}
         <div>
           <h2
             className={`${fraunces.className} text-[32px] md:text-[40px] font-extrabold leading-tight mb-4`}
@@ -41,27 +39,31 @@ export default function JoinWaitList() {
           </p>
         </div>
 
-        
-        <div className="flex flex-row items-start gap-8">
-          <div className="flex flex-col items-center gap-2">
-            <img src="/images/envelope.png" className="w-10 h-10 md:w-12 md:h-12" />
+        {/* Contact - Make in 1 Row in Mobile */}
+        <div className="flex flex-row flex-wrap items-center gap-4 md:gap-8">
+
+          {/* Email */}
+          <div className="flex flex-row items-center gap-2">
+            <img src="/images/envelope.png" className="w-6 h-6 md:w-12 md:h-12" />
             <a
               href="mailto:contact@vaachi.org"
-              className={`${fraunces.className} text-[15px] md:text-[17px] hover:underline`}
+              className={`${fraunces.className} text-[14px] md:text-[17px] hover:underline`}
             >
               contact@vaachi.org
             </a>
           </div>
 
-          <div className="flex flex-col items-center gap-2">
-            <img src="/images/phone.png" className="w-10 h-10 md:w-12 md:h-12" />
+          {/* Phone */}
+          <div className="flex flex-row items-center gap-2">
+            <img src="/images/phone.png" className="w-6 h-6 md:w-12 md:h-12" />
             <a
               href="tel:+919170916622"
-              className={`${fraunces.className} text-[15px] md:text-[17px] hover:underline`}
+              className={`${fraunces.className} text-[14px] md:text-[17px] hover:underline`}
             >
               +91 91709 16622
             </a>
           </div>
+
         </div>
 
       </div>
